e3fbb921dd5107a7f27500c701e7cc0c526d1012
Eduardo Castillo <eduardo.infoline@gmail.com>
2025-05-23 10:53:10 -0500

    feat: enhance QuestionsSummary widget with detailed question and answer display

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -11,7 +11,18 @@ class QuestionsSummary extends StatelessWidget {
       children:
           summaryData.map((data) {
             return Row(
-              children: [Text(((data['question'] as int) + 1).toString())],
+              children: [
+                Text(((data['question'] as int) + 1).toString()),
+                Column(
+                  children: [
+                    Text(data['question'] as String),
+                    const SizedBox(height: 5),
+                    Text(data['correct_answer'] as String),
+                    const SizedBox(height: 5),
+                    Text(data['user_answer'] as String),
+                  ],
+                ),
+              ],
             );
           }).toList(),
     );
--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ads_basics/data/questions.dart';
+import 'package:ads_basics/questions_summary.dart';
 
 class ResultsScreen extends StatelessWidget {
   const ResultsScreen({super.key, required this.chosenAnswers});
@@ -32,7 +33,7 @@ class ResultsScreen extends StatelessWidget {
           children: [
             const Text('You answere X out of Y questions correctly!'),
             const SizedBox(height: 30),
-            const Text('List of answers and questions...'),
+            QuestionsSummary(summaryData: getsummaryData()),
             const SizedBox(height: 30),
             TextButton(onPressed: () {}, child: const Text('Restart Quiz!')),
           ],
